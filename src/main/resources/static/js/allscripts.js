e3f7ba8ab313c8505434ded73feeb3c288496c90
GV Prashanth <36256971+gv-prashanth@users.noreply.github.com>
2019-09-18 00:45:41 +0530

    search feature now works as expected

---
+++ src/main/resources/static/js/allscripts.js
@@ -0,0 +1,13 @@
+function checkAndHide(){
+	var x = document.getElementsByClassName("square");
+	var valueToSearch = document.getElementById("search").value;
+	var i;
+	for (i = 0; i < x.length; i++) {
+	  var valueInBox = x[i].textContent;
+	  if(!valueInBox.toLowerCase().includes(valueToSearch.toLowerCase())){
+		  x[i].style.display = "none";
+	  }else{
+		  x[i].style.display = "inline";
+	  }
+	}
+}
