24b4c71396cea547474cb6b44cb9c0af299255eb
Gumma <3628612@lowes.com>
2021-01-18 18:36:37 +0530

    correcting the typo

--- src/main/resources/static/js/allscripts.js
+++ src/main/resources/static/js/allscripts.js
@@ -2,9 +2,9 @@ function checkAndHide(){
 	var x = document.getElementsByClassName("square");
 	var valueToSearch = document.getElementById("search").value;
 	if(valueToSearch.toLowerCase().includes("songs from arijit singh"))
-	  window.location.href = 'https://v-talk-site.herokuapp.com/testi.html' + '?sender=arijit singh&receiver=shreya ghoshal';
-	if(valueToSearch.toLowerCase().includes("songs from shreya ghoshal"))
 	  window.location.href = 'https://v-talk-site.herokuapp.com/testi.html' + '?sender=shreya ghoshal&receiver=arijit singh';
+	if(valueToSearch.toLowerCase().includes("songs from shreya ghoshal"))
+	  window.location.href = 'https://v-talk-site.herokuapp.com/testi.html' + '?sender=arijit singh&receiver=shreya ghoshal';
 	for (var i = 0; i < x.length; i++) {
 	  var valueInBox = x[i].textContent;
 	  if(!valueInBox.toLowerCase().includes(valueToSearch.toLowerCase())){
