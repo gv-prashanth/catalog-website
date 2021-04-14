271dd291b45ebc097f3db8b2acd8231ecc8bc79b
Gumma <3628612@lowes.com>
2021-04-14 23:30:11 +0530

    adding request logger

--- src/main/java/com/vadrin/catalogwebsite/controllers/HomePageController.java
+++ src/main/java/com/vadrin/catalogwebsite/controllers/HomePageController.java
@@ -2,12 +2,15 @@ package com.vadrin.catalogwebsite.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.vadrin.catalogwebsite.models.RepositoryInfo;
+import com.vadrin.catalogwebsite.services.ClientInfoService;
 import com.vadrin.catalogwebsite.services.GitHubService;
 import com.vadrin.catalogwebsite.services.ScreenshotService;
 
@@ -19,9 +22,13 @@ public class HomePageController {
 
 	@Autowired
 	ScreenshotService screenshotService;
+	
+	@Autowired
+	ClientInfoService clientInfoService;
 
 	@GetMapping("/")
-	public String home(Model model) {
+	public String home(HttpServletRequest request, Model model) {
+	  clientInfoService.printClientInfo(request);
 		List<RepositoryInfo> repoInfo = gitHubService.getCatalogInfo();
 		repoInfo.forEach(repositoryInfo -> repositoryInfo
 				.setSite_url(gitHubService.getReadmeInfo(repositoryInfo.getFull_name(), repositoryInfo.getHtml_url())));
@@ -32,8 +39,8 @@ public class HomePageController {
 	}
 
 	@GetMapping("/vadrin")
-	public String vadrin(Model model) {
-		return home(model);
+	public String vadrin(HttpServletRequest request, Model model) {
+		return home(request, model);
 	}
 
 	@GetMapping("/about")
